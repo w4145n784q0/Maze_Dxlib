6718d337a7775ee224de3b4d25d59b15a8e6e644
直人 渡辺 <230080@jc-21.jp>
2025-02-10 18:45:50 +0900

    プロジェクト ファイルを追加します。

---
+++ Enemy.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "./Library/GameObject.h"
+#include "./globals.h"
+
+
+class Enemy :
+    public GameObject
+{
+    Point pos_;
+    bool isAlive_;
+    float speed_;
+    Point nextPos_;
+    DIR forward_;
+public:
+    Enemy();
+    ~Enemy();
+
+    void Update() override;
+    void Draw() override;
+    bool CheckHit(const Rect& me, const Rect& other);
+    void YCloserMove();
+    void XCloserMove();
+    void XYCloserMove();
+    void XYCloserMoveRandom();
+    void RightHandMove();
+
+};
+
